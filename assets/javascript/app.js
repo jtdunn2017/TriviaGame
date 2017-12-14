63d7015ab3191452016f4cba1b5bf1aa4a1f0b8f
Jeff Jordan <jtdunn2017@outlook.com>
2017-12-14 00:18:34 -0500

    Arrays are setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+
+var catMovies = [];
+var catCities = [];
+var catPlaces = [];
+
+
+// set up array for the movies category
+
+catMovies [0] = 'What was the name of the character played by John Candy in "Planes, Trains and Automobiles?',
+                options['Burt Giffin','Del Griffith','Uncle Buck','Jim Ellison'], 
+                'Burt Griffith','planes_trains_autos';
+catMovies [1] = 'Bruce Banner turns into what fictional superhero?',options['The Flash','Aquman','The Torch',
+                'The Hulk'],'The Hulk','hulk';
+catMovies [2] = 'Who played the female lead in the 1990 era movie "Pretty Woman"?',options['Helen Hunt',
+                'Julia Roberts','Sandra Bullock','Cameron Diaz'],'Julia Roberts','pretty-woman';
+catMovies [3] = 'What movie won the best picture oscar in 1940?',options['Of Mice And Men',
+                'The Grapes Of Wrath','Gone With The Wind','The Wizard Of Oz'],'Gone With The Wind';
+
+// set up array for the cities category
+
+catCities [0] = 'What is the capital of Alabama?',
+                options['Montgomery','Mobile','Tuscaloosa','Birmingham'], 
+                'Montgomery','alabamas';
+catCities [1] = 'What is the capital of Louisiana?',options['New Orleans','Baton Rouge','Corpus Christi',
+                'Mobile'],'Baton Rouge','lousiana';
+catCities [2] = 'What is the capital of Missouri?',options['Pierre',
+                'Wichita','St Louis','Jefferson City'],'Jefferson City','missouri';
+catCities [3] = 'What is the capital of Oklahoma?',options['Tulsa',
+                'Wichita','Oklahoma City','St Louis'],'Okalahoma City' ,'oklahoma';
+
+// set up array for the places category
+catPlaces [0] = 'Where would you find "The Arc de Triomphe"?',
+options['Bruges, Belgium','Fontveille, Monaco','Vianden, Luxembourg','Paris, France'], 
+'Paris France','Arc-de-Triomphe';
+catPlaces [1] = 'Which area of Egypt has been looted of ancient antiquities?',
+options['Cairo','Minya','Alexandria','The Valley of the Kings'],'The Valley of the Kings','valley_of_the_kings';
+catPlaces [2] = 'Where is the Taj Mahal?',options['Pakistan',
+'India','Afghanistan','Iran'],'India','Taj_Mahal';
+catPlaces [3] = 'Where would you find the original Parthenon?',options['Rome',
+'Venice','Greece','Florence'],'Greece' ,'parthenon';
+
+
+               
